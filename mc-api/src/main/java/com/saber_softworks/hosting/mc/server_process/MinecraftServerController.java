a5cabf37e29aaf1227e307e0a7fb48f5110bf6d5
MtgSaber <andrewja314@gmail.com>
2023-06-22 15:07:33 -0400

    Developing architecture

--- mc-api/src/main/java/com/saber_softworks/hosting/mc/server_process/MinecraftServerController.java
+++ mc-api/src/main/java/com/saber_softworks/hosting/mc/server_process/MinecraftServerController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicReference;
 import com.saber_softworks.hosting.mc.server_process.MinecraftServerProcessState.*;
+import io.vavr.control.Option;
 import org.tinylog.Logger;
 
 public class MinecraftServerController {
@@ -28,15 +29,17 @@ public class MinecraftServerController {
         taskQueue.execute(() -> {
             try {
                 modificationSem.acquire();
-                var currentState = presentedStateBox.getAndUpdate(ignored -> actualStateBox.updateAndGet(
+                var currentState = actualStateBox.getAndUpdate( // TODO: this is wrong
                         state -> switch (state) {
                             case StartingPoint startingPoint -> new Initializing(configPath);
                             case MinecraftServerProcessState other -> other;
                         }
-                ));
+                );
+                presentedStateBox.set(actualStateBox.get());
+                checkpointSem.release();
                 if (currentState instanceof StartingPoint startingPoint) {
-                    var initializationResult = this.serverProcess.initialize(startingPoint, configPath);
-                    // TODO: finish this
+                    actualStateBox.set(this.serverProcess.initialize(startingPoint, configPath));
+                    presentedStateBox.set(actualStateBox.get());
                 }
             } catch (InterruptedException e) {
                 Logger.warn(e);
@@ -44,6 +47,15 @@ public class MinecraftServerController {
                 modificationSem.release();
             }
         });
+        try {
+            checkpointSem.acquire();
+        } catch (InterruptedException e) {
+            Logger.warn(e);
+        }
         return getState();
+    }
+
+    private Option<InterruptedException> attemptScheduledModification() {
+        return null; // TODO: implement
     }
 }
--- mc-api/src/main/java/com/saber_softworks/hosting/mc/server_process/MinecraftServerProcess.java
+++ mc-api/src/main/java/com/saber_softworks/hosting/mc/server_process/MinecraftServerProcess.java
@@ -3,5 +3,5 @@ package com.saber_softworks.hosting.mc.server_process;
 import com.saber_softworks.hosting.mc.server_process.MinecraftServerProcessState.*;
 
 public interface MinecraftServerProcess {
-    MinecraftServerProcessState initialize(StartingPoint previousState, String configPath);
+    MinecraftServerProcessState initialize(StartingPoint previousState, String configPath); // TODO: remove this and use config lib instead
 }
